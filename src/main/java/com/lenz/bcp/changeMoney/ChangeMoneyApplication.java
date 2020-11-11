07dc977a1d53812f0b6318f78b32c5a57ac386a4
Lizardo <lenz.90@gmail.com>
2020-11-11 15:43:43 -0500

    first commit

---
+++ src/main/java/com/lenz/bcp/changeMoney/ChangeMoneyApplication.java
@@ -0,0 +1,13 @@
+package com.lenz.bcp.changeMoney;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ChangeMoneyApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ChangeMoneyApplication.class, args);
+	}
+
+}
